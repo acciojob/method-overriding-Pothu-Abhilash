328e063baa91f84a66c1717810f8a02182963ddb
Pothu-Abhilash <abhipothu2000@gmail.com>
2024-04-10 12:31:33 +0530

    Initial commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
+import java.util.*;
 public class Main {
-  
+  B obj = new B();
+    String result = obj.meth();
+}
+class A{
+    public static String meth()
+    {
+        String ans = "Invoking method from class A";
+
+        return ans;
+    }
+}
+class B extends A{
+    public static String meth()
+    {
+        String ans = "Method is overridden in Extendend class B";
+        return ans;
+    }
 }
